cea9bb91a257d064918c7bdc1c079fd19a0d346a
Vivek Bhos <bhosvivek123@gmail.com>
2025-05-12 17:25:57 +0530

    Refactor home page components by removing the PricingSection and updating the Features and Services sections. Enhanced features with new images and descriptions, streamlined service offerings, and improved layout for better user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <ProcessSection />
     
       <TestimonialsSection />
-      <PricingSection />
+    
       <FAQSection />
       <ContactSection />
      
