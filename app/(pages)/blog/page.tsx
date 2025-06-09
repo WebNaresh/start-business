c0959fbea9be6480635fab800e4662b189b44e54
Vivek Bhos <bhosvivek123@gmail.com>
2025-06-09 14:47:08 +0530

    Remove ChatButton component from BlogPage for a cleaner layout and improved focus on blog content.

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,11 +1,7 @@
 import BlogHero from './_components/blog-hero'
-
-
 import BlogFeatured from './_components/blog-featured'
 import BlogCategories from './_components/blog-categories'
 import BlogGrid from './_components/blog-grid'
-
-import ChatButton from '@/app/components/chat-button'
 
 export default function BlogPage() {
   return (
@@ -14,8 +10,6 @@ export default function BlogPage() {
       <BlogFeatured />
       <BlogCategories />
       <BlogGrid />
-
-      <ChatButton />
     </div>
   )
 } 
