23bdfb4407b4d7273da2fb5e0dd70be377d4216e
Vivek Bhos <bhosvivek123@gmail.com>
2025-06-18 16:45:24 +0530

    refactor: Simplify Blog type export by removing editorData extension

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,2 @@
-import { Blog as PrismaBlog } from '@prisma/client'
-
-// Extend the Prisma Blog type to include editorData field
-export interface Blog extends PrismaBlog {
-    editorData?: string | null
-}
+import { Blog } from '@prisma/client'
+export type { Blog }
